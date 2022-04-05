98403acb4ed5c52e4305cd9e6df0e2f195274343
gichuhiii <wangui910@gmail.com>
2022-04-06 01:00:40 +0300

    chat route updated

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get("/chat",[ChatController::class,"chat"]);
+
+
